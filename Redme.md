8f5ccbc9f2418a706664da26d8fc975a46d0fdb9
arifbiswas <mxgangstar310@gmail.com>
2022-12-17 00:20:38 +0600

    finished

---
+++ Redme.md
@@ -0,0 +1,120 @@
+1. Register
+Online Registration API: https://atg-world-sever-arifbiswas.vercel.app/v1/register
+Local Registration API: http://localhost:5000/v1/register
+Method: Post
+Get Warning : 3
+message : "Email already exists"
+
+message: "Registration successfully"
+
+message: "Registration failed"
+
+Need data like this (JSON):
+{
+    "firstName" : "Demo",
+    "lastName" : " Name",
+    "email" : "demo@gamil.com",
+    "password" : "demoPassword"
+}
+
+2. Login
+Online Registration API: https://atg-world-sever-arifbiswas.vercel.app/v1/login
+Local Registration API: http://localhost:5000/v1/login
+Method: Post
+Get warning : 4
+message : "Successfully"
+
+message : "Password is correct"
+
+message : "'User Not Found'. Please Create a new account"
+
+message : "Password have not found"
+
+Need data like this (JSON):
+{
+    "email" : "demo@gamil.com",
+    "password" : "demoPassword"
+}
+
+3. Forget Password
+Online Registration API: https://atg-world-sever-arifbiswas.vercel.app/v1/forgetPassword
+Local Registration API: http://localhost:5000/v1/forgetPassword
+Method: Post
+Get warning : 2
+message:`forget your password don't worry your password is ${dbUsers.password}`
+
+message : "incorrect email address"
+
+Need data like this (JSON):
+{
+    "email" : "demo@gamil.com"
+}   
+
+
+End Login Part 
+
+CRUD
+4.Create: Add New Post
+Online Registration API: https://atg-world-sever-arifbiswas.vercel.app/v1/addPost
+Local Registration API: http://localhost:5000/v1/addPost
+Method: Post
+Get warning : 2
+ message : "Post add successfully"
+
+ message : "Post add failed"
+
+Need data like this (JSON):
+{
+    "title": "Demo is Ready to Test",
+    "description": "This is an example of how to test the application."
+}
+
+
+
+5. Read: Get all post
+
+Online Registration API: https://atg-world-sever-arifbiswas.vercel.app/v1/allPost
+Local Registration API: http://localhost:5000/v1/allPost
+Method: Get
+
+6. Delete: Delete post by id
+Online Registration API: https://atg-world-sever-arifbiswas.vercel.app/v1/delete/:id
+Local Registration API: http://localhost:5000/v1/delete/:id
+Method: Delete
+
+7. Update: Update post by id
+Online Registration API: https://atg-world-sever-arifbiswas.vercel.app/v1/updatePost/:id
+Local Registration API: http://localhost:5000/v1/updatePost/:id
+Method: Patch
+Need data like this (JSON):
+{
+    "title": "Demo is Ready to Test",
+    "description": "This is an example of how to test the application."
+}
+
+
+8. Like: Like post by id
+Online Registration API: https://atg-world-sever-arifbiswas.vercel.app/v1/like/:id?email=demo@gmail.com
+Local Registration API: http://localhost:5000/v1/like/:id?email=demo@gmail.com
+Method: Post
+
+9. Comment:Add new Comment 
+Online Registration API: https://atg-world-sever-arifbiswas.vercel.app/v1/comments
+Local Registration API: http://localhost:5000/v1/comments
+Method: Post
+
+
+
+Need data like this (JSON):
+{
+    "postId": "",
+    "comments": "text and comments"
+}
+
+10. Comment: GET All Comment particular posts by id
+Online Registration API: https://atg-world-sever-arifbiswas.vercel.app/v1/comments/:postId
+Local Registration API: http://localhost:5000/v1/comments/:postId
+Method: GET
+
+Done 
+Please give my feedback
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 app.use(express.json())
@@ -13,25 +13,204 @@ const Client = new MongoClient(uri)
 async function run(){
     try {
         const UserCollection = Client.db("AtgWorld").collection('users');
+        const PostsCollection = Client.db("AtgWorld").collection('posts');
+        const CommentsCollection = Client.db("AtgWorld").collection('comments');
 
-        app.post('/v1/users',async(req,res)=>{
-            // console.log(req.body);
+        // Logs Methods of Login and registration or Forgot Password
+        // registration
+        app.post('/v1/register',async(req,res)=>{
             try {
+                const dbUsers = await UserCollection.findOne({email : req.body.email})
+                if(dbUsers) {
+                     res.status(400).json({
+                        message : "Email already exists"})
+                }
                 const result = await UserCollection.insertOne(req.body)
-                res.send(result)
+               if(result.acknowledged){
+                res.send({message: "Registration successfully"})
+               }
+               else{
+                res.send({message: "Registration failed"})
+               }
+                
             } catch (error) {
                 console.log(error.message);
             }
         })
-        app.get('/v1/users',async(req,res)=>{
+        // Login
+        app.post('/v1/login',async(req,res)=>{
             try {
-                const users = await UserCollection.find().toArray()
-                res.send(users)
+                const email = req.body.email;
+                const password = req.body.password;
+                // console.log(email, password);
+                const dbUsers = await UserCollection.findOne({email : email})
+                console.log(dbUsers);
+                if(password){
+                    if(dbUsers){
+                        const dbPassword = dbUsers.password;
+                        if(dbPassword === password) {
+                            res.send({message : "Successfully"});
+                        } else{
+                            res.send({message : "Password is correct"});
+                        }
+    
+                    } else{
+                        res.send({message : "'User Not Found'. Please Create a new account"});
+                    }
+                   } else{
+                res.send({message : "Password have not found"});
+               }
             } catch (error) {
             }
         })
+        // Forgot Password
+        app.post('/v1/forgetPassword',async(req,res)=>{
+            try {
+                const email = req.body.email;
+            const dbUsers = await UserCollection.findOne({email : email})
+           if(dbUsers?.email === email){
+                res.send({message:`Your password is ${dbUsers.password}`});
+               
+           }
+            res.send({message : "incorrect email address"});
+            } catch (error) {
+                console.log(error);
+            }
+        })
+
+        // Post Curd operations
+        // Read 
+        app.get('/v1/allPost', async (req, res) => {
+            try {
+                const posts = await PostsCollection.find().toArray();
+                res.send(posts);
+            } catch (error) {
+                console.log(error.message);
+            }
+        })
+        // Create 
+        app.post('/v1/addPost',async(req,res)=>{
+          try {
+            const post = req.body;
+            const result = await PostsCollection.insertOne(post);
+            if(result.acknowledged){
+
+                res.send({  message : "Post add successfully"})
+            }
+            else{
+                res.send({  message : "Post add failed"});
+            }
+          } catch (error) {
+            console.log(error.message);
+          }
+        })
+        // Delete 
+        app.delete("/v1/delete/:id", async(req, res) => {
+            try {
+                const id = req.params.id;
+                const result = await PostsCollection.deleteOne({_id : ObjectId(id)});
+                if(result.deletedCount > 0){
+                    res.send({  message : "Post delete successfully"});
+                }
+               else{
+                res.send({ message : "Post delete failed"});
+               }
+            } catch (error) {
+                
+            }
+        })
+        // Update 
+        app.patch('/v1/updatePost/:id',async(req,res)=>{
+            try {
+                const id = req.params.id;
+                const query = {_id : ObjectId(id)}
+                const result = await PostsCollection.updateOne(query,{$set:{
+                    title : req.body.title,
+                    describe : req.body.describe
+                }})
+                if(result.modifiedCount > 0){
+                    res.send({  message : "Post update successfully"});
+                }
+                else{
+                    res.send({  message : "Post update failed"});
+                }
+                
+            } catch (error) {
+                
+            }
+        })
+        // Post like 
+        app.patch('/v1/like/:id', async (req, res) => {
+            try {
+                const id = req.params.id;
+                console.log(req.query.email);
+                const query = {_id : ObjectId(id)}
+                const dbPost = await PostsCollection.findOne(query);
+                if(!dbPost.like){
+                    const result = await PostsCollection.updateOne(query,{$set:{
+                        likedEmail : [req.query?.email],
+                        like : 1
+                    }})
+                    if(result.modifiedCount > 0){
+                        res.send({  message : "liked successfully"});
+                    }
+                    else{
+                        res.send({  message : "failed"});
+                    }
+                }
+                else{
+                   if(dbPost?.likedEmail && dbPost?.likedEmail.includes(req?.query?.email)){
+                    res.send({  message : "already liked"});
+                   }
+                   else{
+                    const like = dbPost.like;
+
+                    const result = await PostsCollection.updateOne(query,{$set:{
+                        likedEmail : [...dbPost?.likedEmail,req.query?.email],
+                        like : like + 1
+                    }})
+                    if(result.modifiedCount > 0){
+                        res.send({  message : "liked successfully"});
+                    }
+                    else{
+                        res.send({  message : "failed"});
+                    }
+                   }
+                }
+            } catch (error) {
+                console.log(error);
+                res.send(error);
+            }
+        })
+
+          // Comment 
+          app.get('/v1/comments/:postId', async (req, res) => {
+            try {
+                const id = req.params.postId;
+                const query = {postId : id}
+                const comments = await CommentsCollection.find(query).toArray();
+                 res.send(comments);
+            } catch (error) {
+                console.log(error);
+            }
+        })
+        app.post("/v1/comments", async(req , res)=>{
+         try {
+            const comment = await CommentsCollection.insertOne(req.body);
+            if(comment.acknowledged){
+                res.send({  message : "Comment added successfully"});
+            }else{
+                res.send({  message : "Comment added failed"});
+            }
+         } catch (error) {
+            console.log(error);
+         }
+        })
+      
+
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
+        res.status(500).send(error);
     }
 }
 run().catch(error => console.log(error));
